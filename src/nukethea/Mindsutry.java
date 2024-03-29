a0b4aaae0fd4f95fc191b5e39fa27421f0a8ffbd
Leo <86385005+Leo-MathGuy@users.noreply.github.com>
2024-03-29 17:52:05 +0000

    new texture

--- src/nukethea/Mindsutry.java
+++ src/nukethea/Mindsutry.java
@@ -1,29 +1,24 @@
 package nukethea;
 
 import arc.*;
+import arc.struct.Seq;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
+import mindustry.type.Weapon;
 import mindustry.ui.dialogs.*;
 
 public class Mindsutry extends Mod {
 
     public Mindsutry(){
         Log.info("Loaded Mindsutry");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("Impostor sus me harder").row();
-                dialog.cont.button("[scarlet]Eat the impostor[]", dialog::hide).size(400f, 25f);
-                dialog.show();
-            });
-        });
+        for (int i = 1; i < 5; i++) {
+            UnitTypes.vanquish.weapons.pop();
+        }
+        
     }
 
 }
