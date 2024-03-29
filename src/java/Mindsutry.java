dcc09ab64bd9b634fa7cd86bc08373f41d7f92ec
Leo <86385005+Leo-MathGuy@users.noreply.github.com>
2024-03-29 16:11:20 +0000

    vanquish

--- src/java/Mindsutry.java
+++ src/java/Mindsutry.java
@@ -12,25 +12,18 @@ import mindustry.ui.dialogs.*;
 public class ExampleJavaMod extends Mod{
 
     public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+        Log.info("Loaded Mindsutry");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.add("Impostor sus me harder").row();
+                dialog.cont.button("[scarlet]Eat the impostor", dialog::hide).size(400f, 25f);
                 dialog.show();
             });
         });
-    }
-
-    @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
     }
 
 }
