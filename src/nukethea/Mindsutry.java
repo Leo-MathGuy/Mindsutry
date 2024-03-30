117cc8dd17b61e921a2af6b88b368f90e3e89942
Leo <86385005+Leo-MathGuy@users.noreply.github.com>
2024-03-30 08:45:20 +0000

    more

--- src/nukethea/Mindsutry.java
+++ src/nukethea/Mindsutry.java
@@ -1,25 +1,16 @@
 package nukethea;
 
-import arc.*;
-import arc.struct.Seq;
 import arc.util.*;
-import mindustry.*;
 import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.type.Weapon;
-import mindustry.ui.dialogs.*;
 
 public class Mindsutry extends Mod {
 
     public Mindsutry(){
         Log.info("Loaded Mindsutry");
         for (int i = 1; i < 5; i++) {
-            let weapon = UnitTypes.vanquish.weapons.get(1);
-            weapon.outlineColor = Pal.darkOutline;
-            UnitTypes.vanquish.weapons.remove(1);
-            UnitTypes.vanquish.weapons.insert(1, weapon);
+            UnitTypes.vanquish.weapons.get(i).top = true;
+            UnitTypes.vanquish.weapons.get(i).shootY = -0.5f;
         }
         
     }
