99ff9e9c76662631b995ec0627fafaf42e89db49
Leo <86385005+Leo-MathGuy@users.noreply.github.com>
2024-03-30 09:02:34 +0000

    amogus

--- src/nukethea/Mindsutry.java
+++ src/nukethea/Mindsutry.java
@@ -6,13 +6,16 @@ import mindustry.mod.*;
 
 public class Mindsutry extends Mod {
 
-    public Mindsutry(){
+    public Mindsutry() {
         Log.info("Loaded Mindsutry");
+        
+    }
+
+    public void loadContent() {
+        Log.info("Loading vanquish mods...");
         for (int i = 1; i < 5; i++) {
             UnitTypes.vanquish.weapons.get(i).top = true;
             UnitTypes.vanquish.weapons.get(i).shootY = -0.5f;
         }
-        
     }
-
 }
